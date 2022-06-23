7a00ad6874d00c6c599bc7ec62f4b184c9a7466e
Zach Underwood <zawood11@gmail.com>
2022-06-22 22:42:48 -0400

    add basic validations to exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,8 @@
 class Exercise < ApplicationRecord
+    t.belongs_to :workout
+    t.belongs_to :movement
+
+    validates :sets, presence: true
+    validates :reps, presence: true
+    validates :rest_interval, presence: true
 end
