6651ccaf86ca8bdbf4db50925452df393b90f7eb
Zach Underwood <zawood11@gmail.com>
2022-07-13 18:02:57 -0400

    add index and destroy methods for users in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     get "/me", to: "users#show"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    resources :users, only: [:index, :destroy]
 
     
   end
